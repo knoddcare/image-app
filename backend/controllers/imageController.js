1e9b106575bbdd728eb127c30d699fc7d084a464
Max Witt <kirayatail@gmail.com>
2021-04-28 02:32:47 +0200

    task 2. Add file filter for JPEG/PNG, Resize images to 400x400px, format filename by specification. See README for details.

--- backend/controllers/imageController.js
+++ backend/controllers/imageController.js
@@ -1,7 +1,27 @@
+const { fail } = require('assert');
+const fs = require('fs');
 const multer = require("multer");
+const sharp = require('sharp');
 const ImageMetadata = require("../models/ImageMetadataModel");
 
 const IMG_DIRECTORY_PATH = "public/img";
+
+const formatFileName = (fn) => {
+  const pad = (t) => ('' + t).padStart(2, '0');
+
+  const date = new Date();
+  return fn.toLowerCase()
+    .substr(0,10)
+    .replace(/\s+/g,'-')
+    .replace(/[åä]/g, 'a')
+    .replace(/ö/g, 'o') +
+    date.getFullYear() +
+    pad(1 + date.getMonth()) +
+    pad(date.getDate()) +
+    pad(date.getHours()) +
+    pad(date.getMinutes()) +
+    pad(date.getSeconds());
+}
 
 const multerStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -9,11 +29,18 @@ const multerStorage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     const extension = file.mimetype.split("/")[1];
-    cb(null, `${req.body.name}.${extension}`);
+    const filename = formatFileName(file.originalname);
+    cb(null, `${filename}.${extension}`);
   },
 });
 
+const imageFileFilter = (req, file, cb) => {
+  cb(null, file.mimetype === 'image/jpeg' || 
+           file.mimetype === 'image/png');
+}
+
 const upload = multer({
+  fileFilter: imageFileFilter,
   storage: multerStorage,
 });
 
@@ -28,7 +55,44 @@ exports.getAllImages = async (req, res, next) => {
 
 exports.uploadImage = upload.single("photo");
 
+exports.resizeImage = (req, res, next) => {
+  if(!req.file) {
+    return next();
+  }
+
+  const fail = (err) => {
+    return res.send(500).json({
+      status: "fail",
+      message: "Something went wrong when saving your image"
+    });
+  }
+  
+  sharp(req.file.path)
+    .resize(400, 400)
+    .toBuffer((err, buf) => {
+      
+      if (err) {
+        return fail();
+      }
+      fs.writeFile(req.file.path, buf, (error, sharp) => {
+        if(error) {
+          return fail();
+        }
+
+        return next();
+      });
+    });
+}
+
 exports.createImageMetadata = async (req, res, next) => {
+  
+  if (req.file == null) {
+    return res.status(415).json({
+      status: "fail",
+      message: "Image must be PNG or JPEG"
+    });
+  }
+
   const doc = await ImageMetadata.create({
     name: req.body.name,
     path: `/img/${req.file.filename}`,
