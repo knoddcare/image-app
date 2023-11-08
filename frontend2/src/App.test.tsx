12742eaabcb2c10319da6a2ff02de219f98540d8
John Soukka <john@knodd.se>
2023-11-08 11:35:13 +0100

    Create Vite app

---
+++ frontend2/src/App.test.tsx
@@ -0,0 +1,9 @@
+import { test, expect } from "vitest";
+import { render } from "@testing-library/react";
+import App from "./App";
+
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+  const title = getByText(/Image Uploading App/i);
+  expect(title).toBeInTheDocument();
+});
