8d7556edb8d1819988f6c8d059a27329d397d396
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2023-11-17 11:57:36 +0100

    Implement service class to connect to backend

---
+++ frontend/src/services/Api.ts
@@ -0,0 +1,35 @@
+import axios from "../axios/config";
+import { AxiosProgressEvent, AxiosResponse } from "axios";
+
+export type ProgressCallback = (event: AxiosProgressEvent) => void;
+
+export interface ImageModel {
+  name: string;
+  path: string;
+}
+
+class ApiService {
+  upload(
+    file: File,
+    name: string,
+    onUploadProgress: ProgressCallback = () => {},
+  ): Promise<AxiosResponse<void>> {
+    const formData = new FormData();
+    // It is important that file data is added last.
+    // See: https://github.com/expressjs/multer/issues/322
+    formData.append("name", name);
+    formData.append("photo", file);
+    return axios.post("/images", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      onUploadProgress,
+    });
+  }
+
+  getFiles(): Promise<AxiosResponse<{ data: ImageModel[] }>> {
+    return axios.get("/images");
+  }
+}
+
+export default new ApiService();
