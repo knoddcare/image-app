fed9f4049f19ee82bd3e785bf78142f575a94c4a
Niklas <niklasjsson@gmail.com>
2023-11-28 23:11:27 +0100

    adds name transform

--- backend/controllers/imageController.js
+++ backend/controllers/imageController.js
@@ -1,32 +1,48 @@
 const multer = require("multer");
+const moment = require("moment");
 const ImageMetadata = require("../models/ImageMetadataModel");
 
 const IMG_DIRECTORY_PATH = "public/img";
+
+const validNameRegex = /[^a-z,\-]+/ig;
+
+const getValidExtension = file => {
+  const extension = file.mimetype.split("/")[1];
+
+  switch (extension) {
+    case 'png':
+    case 'jpg':
+    case 'jpeg':
+      return extension;
+    default:
+      return;
+  }
+};
+
+const getValidFileName = name => {
+  return name
+    .replaceAll(' ', '-')
+    .replace(validNameRegex, '')
+    .slice(0, 10);
+};
 
 const multerStorage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, IMG_DIRECTORY_PATH);
   },
   filename: (req, file, cb) => {
-    const extension = file.mimetype.split("/")[1];
+    const extension = getValidExtension(file);
+    if (!extension) cb(`extension not valid, ${extension}`);
 
-    switch (extension) {
-      case 'png':
-      case 'jpg':
-      case 'jpeg':
-        break;
-      default:
-        console.error(`extension not valid, ${extension}`);
-        return;
-    }
+    const fileName = `${getValidFileName(req.body.name)}_${moment().format("YYYYMMDDHHmmss")}`;
 
-    cb(null, `${req.body.name}.${extension}`);
+    cb(null, `${fileName}.${extension}`);
   },
 });
 
 const upload = multer({
   storage: multerStorage,
-});
+}).single("photo");
 
 exports.getAllImages = async (req, res, next) => {
   const data = await ImageMetadata.find();
@@ -37,23 +53,30 @@ exports.getAllImages = async (req, res, next) => {
   });
 };
 
-exports.uploadImage = upload.single("photo");
-
 exports.createImageMetadata = async (req, res, next) => {
-  const doc = await ImageMetadata.create({
-    name: req.body.name,
-    path: `/img/${req.file.filename}`
-  });
+  return upload(req, res, async (err) => {
+    if (err) {
+      return res.status(400).json({
+        status: "fail",
+        message: "file input"
+      });
+    }
 
-  if (!doc) {
-    return res.status(400).json({
-      status: "fail",
-      message: "invalid input"
+    const doc = await ImageMetadata.create({
+      name: req.body.name,
+      path: `/img/${req.file.filename}`
     });
-  }
-
-  return res.status(201).json({
-    status: "success",
-    data: doc
+  
+    if (!doc) {
+      return res.status(400).json({
+        status: "fail",
+        message: "db input"
+      });
+    }
+  
+    return res.status(201).json({
+      status: "success",
+      data: doc
+    });
   });
 };
